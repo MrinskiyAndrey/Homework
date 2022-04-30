a022d5604d86537a54159b16cf2dcab2bd0742b6
MrinskiyAndrey <97047164+MrinskiyAndrey@users.noreply.github.com>
2022-04-30 12:45:29 +0300

    Деление строки на гласны и согласные

--- chernovik.py
+++ chernovik.py
@@ -1,16 +1,19 @@
+
 print('Введите строку кириллицей :\n')
-text = input().upper().split()
+text = str(input().upper().split())
 
 def func(t):
-    glas = {'А', 'Е', 'Ё', 'И', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я'}
-    soglas = {'Б', 'В', 'Г', 'Д', 'Ж', 'З', 'Й', 'К', 'Л', 'М', 'Н', 'П', 'Р', 'С', 'Т', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ь'}
+    glas = 'АЕЁИОУЫЭЮЯ'
+    soglas = 'БВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬ'
     str1 = []
     str2 = []
-    for i in t:
-        if i == glas:
-            str1.append(i)
-        elif i == soglas:
-            str2.append(i)
-        print('Гласные: \n', str1, '\nсогласные: \n', str2)
-
+    for i in glas:
+        if t.find(i):
+             str1.append(i)
+    
+    for i in soglas:
+        if t.find(i):
+             str2.append(i)
+    print('Гласные: \n', str1, '\nсогласные: \n', str2)
+    
 func(text)
